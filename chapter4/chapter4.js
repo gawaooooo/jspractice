189b516f2ae01e54d9797c2a3e77fb00513b463b
Kanako Ogawa <gawao57@gmail.com>
2013-02-12 16:28:28 +0900

    4.3.1 P.33

--- chapter4/chapter4.js
+++ chapter4/chapter4.js
@@ -4,3 +4,11 @@
 var add = function(a, b) {
 	return a + b;
 };
+
+// 4.3.1 メソッドの呼び出しパターン
+var myObject = {
+	value: 0,
+	increment: function(inc) {
+		this.value += typeof inc === 'number' ? inc : 1;
+	}
+};
